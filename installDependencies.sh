3a86bbd55d9569104d2d367ddf8b52fa3a653429
Vinzenz Eck <vinzenz.g.eck@ntnu.no>
2014-12-17 14:34:26 +0000

    updated installeDependencies for ubuntu

--- installDependencies.sh
+++ installDependencies.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
 
-if [ -f /etc/lsb-release ]; then
-    . /etc/lsb-release
-    OS=$DISTRIB_ID
-    VER=$DISTRIB_RELEASE
-	echo " no auto installation for detected system available"
-
-elif [ -f /etc/debian_version ]; then
+if [ -f /etc/debian_version ]; then
     OS=debian-ubuntu 
-    VER=$(cat /etc/debian_version)
     echo " %OS detected, starting installation .."
     
+    apt-get -y install python-pip
     apt-get -y install build-essential python-dev
-	apt-get -y install pygtk2
-	apt-get -y install scipy
-	apt-get -y install python-matplotlib
+    apt-get -y install pygtk2
+    apt-get -y install scipy
+    apt-get -y install python-matplotlib
 
 elif [ -f /etc/redhat-release ]; then
     # TODO add code for Red Hat and CentOS here
@@ -23,12 +17,12 @@ elif [ -f /etc/redhat-release ]; then
     echo $OS "detected, starting installation .."
     
     yum -y install gcc
-	yum -y install gcc-gfortran
-	yum -y install gcc-c++
-	yum -y install python-devel
-	yum -y install pygtk2
-	yum -y install scipy
-	yum -y install python-matplotlib
+    yum -y install gcc-gfortran
+    yum -y install gcc-c++
+    yum -y install python-devel
+    yum -y install pygtk2
+    yum -y install scipy
+    yum -y install python-matplotlib
     
 else
     OS=$(uname -s)
