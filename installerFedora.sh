1de60f4f0a777b967b7449b4148b41e48c30d7a0
Vinzenz Eck <vinzenz.g.eck@ntnu.no>
2014-12-17 09:52:15 +0000

    added installer bash script for dependencies

---
+++ installerFedora.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+yum -y install gcc
+yum -y install gcc-gfortran
+yum -y install gcc-c++
+yum -y install python-devel
+yum -y install pygtk2
+yum -y install scipy
+yum -y install python-matplotlib
+
+pip install -r requirements.txt
