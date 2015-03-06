d4e728f5875aa73739b60c8f9c07ca34b0e14915
Frederik <none>
2015-03-06 13:28:45 +0000

    "Added some  BCs: ExpFunc, CCAInflow, ThoAInflow and AoBifInflow"

--- NetworkLib/classBoundaryConditions.py
+++ NetworkLib/classBoundaryConditions.py
@@ -401,12 +401,62 @@ class ExpFunc(BoundaryConditionType1):
 	"""	
 	Boundary profile - type 1
 	
-		creates an exponentialfunction according to Comparison1Dscheme
+		creates an exponentialfunction according to section 3.1 Comparison1Dscheme
 	"""
 	def function1(self, t, t0, pulsNum):
+		
 		return 0.000001*np.exp(-10000*pow((t-0.05),2))
 	
 	
+class CCAInflow(BoundaryConditionType1):
+	"""
+	Creates the inflow used in test Of Common Carotid artery section 3.2 in Comparison1Dscheme 
+	"""
+	def __init__(self):
+		
+		BoundaryConditionType1.__init__(self)
+				
+		# # additional variables for this function
+		self.T = 1.1
+	
+	def function1(self, t, t0, pulsNum):
+
+		return 1e-6*(6.5+3.294*np.sin(2*np.pi*t/self.T-0.023974)+1.9262*np.sin(4*np.pi*t/self.T-1.1801)-1.4219*np.sin(6*np.pi*t/self.T+0.92701)-0.66627*np.sin(8*np.pi*t/self.T-0.24118)-0.33933*np.sin(10*np.pi*t/self.T-0.27471)-0.37914*np.sin(12*np.pi*t/self.T-1.0557)+0.22396*np.sin(14*np.pi*t/self.T+1.22)+0.1507*np.sin(16*np.pi*t/self.T+1.0984)+0.18735*np.sin(18*np.pi*t/self.T+0.067483)+0.038625*np.sin(20*np.pi*t/self.T+0.22262)+0.012643*np.sin(22*np.pi*t/self.T-0.10093)-0.0042453*np.sin(24*np.pi*t/self.T-1.1044)-0.012781*np.sin(26*np.pi*t/self.T-1.3739)+0.014805*np.sin(28*np.pi*t/self.T+1.2797)+0.012249*np.sin(30*np.pi*t/self.T+0.80827)+0.0076502*np.sin(32*np.pi*t/self.T+0.40757)+0.0030692*np.sin(34*np.pi*t/self.T+0.195)-0.0012271*np.sin(36*np.pi*t/self.T-1.1371)-0.0042581*np.sin(38*np.pi*t/self.T-0.92102)-0.0069785*np.sin(40*np.pi*t/self.T-1.2364)+0.0085652*np.sin(42*np.pi*t/self.T+1.4539)+0.0081881*np.sin(44*np.pi*t/self.T+0.89599)+0.0056549*np.sin(46*np.pi*t/self.T+0.17623)+0.0026358*np.sin(48*np.pi*t/self.T-1.3003)-0.0050868*np.sin(50*np.pi*t/self.T-0.011056)-0.0085829*np.sin(52*np.pi*t/self.T-0.86463))
+		
+		
+class AortaInflow(BoundaryConditionType1):
+	"""
+	Creates the inflow used in test Of Single Aorta used in section 3.3 in Comparison1Dscheme 
+	"""
+	def __init__(self):
+		
+		BoundaryConditionType1.__init__(self)
+				
+		# # additional variables for this function
+		self.T = 0.9550
+	
+	def function1(self, t, t0, pulsNum):
+# 		print "self.T is: ", self.T
+# 		print "t is: ", t
+# 		print "return is: ", 4.0*np.arctan(1.0)*0.0126*0.0126*(0.20617+0.37759*np.sin(2*np.pi*t/self.T+0.59605)+0.2804*np.sin(4*np.pi*t/self.T-0.35859)+0.15337*np.sin(6*np.pi*t/self.T-1.2509)-0.049889*np.sin(8*np.pi*t/self.T+1.3921)+0.038107*np.sin(10*np.pi*t/self.T-1.1068)-0.041699*np.sin(12*np.pi*t/self.T+1.3985)-0.020754*np.sin(14*np.pi*t/self.T+0.72921)+0.013367*np.sin(16*np.pi*t/self.T-1.5394)-0.021983*np.sin(18*np.pi*t/self.T+0.95617)-0.013072*np.sin(20*np.pi*t/self.T-0.022417)+0.0037028*np.sin(22*np.pi*t/self.T-1.4146)-0.013973*np.sin(24*np.pi*t/self.T+0.77416)-0.012423*np.sin(26*np.pi*t/self.T-0.46511)+0.0040098*np.sin(28*np.pi*t/self.T+0.95145)-0.0059704*np.sin(30*np.pi*t/self.T+0.86369)-0.0073439*np.sin(32*np.pi*t/self.T-0.64769)+0.0037006*np.sin(34*np.pi*t/self.T+0.74663)-0.0032069*np.sin(36*np.pi*t/self.T+0.85926)-0.0048171*np.sin(38*np.pi*t/self.T-1.0306)+0.0040403*np.sin(40*np.pi*t/self.T+0.28009)-0.0032409*np.sin(42*np.pi*t/self.T+1.202)-0.0032517*np.sin(44*np.pi*t/self.T-0.93316)+0.0029112*np.sin(46*np.pi*t/self.T+0.21405)-0.0022708*np.sin(48*np.pi*t/self.T+1.1869)-0.0021566*np.sin(50*np.pi*t/self.T-1.1574)+0.0025511*np.sin(52*np.pi*t/self.T-0.12915)-0.0024448*np.sin(54*np.pi*t/self.T+1.1185)-0.0019032*np.sin(56*np.pi*t/self.T-0.99244)+0.0019476*np.sin(58*np.pi*t/self.T-0.059885)-0.0019477*np.sin(60*np.pi*t/self.T+1.1655)-0.0014545*np.sin(62*np.pi*t/self.T-0.85829)+0.0013979*np.sin(64*np.pi*t/self.T+0.042912)-0.0014305*np.sin(66*np.pi*t/self.T+1.2439)-0.0010775*np.sin(68*np.pi*t/self.T-0.79464)+0.0010368*np.sin(70*np.pi*t/self.T-0.0043058)-0.0012162*np.sin(72*np.pi*t/self.T+1.211)-0.00095707*np.sin(74*np.pi*t/self.T-0.66203)+0.00077733*np.sin(76*np.pi*t/self.T+0.25642)-0.00092407*np.sin(78*np.pi*t/self.T+1.3954)-0.00079585*np.sin(80*np.pi*t/self.T-0.49973))
+		return 4.0*np.arctan(1.0)*0.0126*0.0126*(0.20617+0.37759*np.sin(2*np.pi*t/self.T+0.59605)+0.2804*np.sin(4*np.pi*t/self.T-0.35859)+0.15337*np.sin(6*np.pi*t/self.T-1.2509)-0.049889*np.sin(8*np.pi*t/self.T+1.3921)+0.038107*np.sin(10*np.pi*t/self.T-1.1068)-0.041699*np.sin(12*np.pi*t/self.T+1.3985)-0.020754*np.sin(14*np.pi*t/self.T+0.72921)+0.013367*np.sin(16*np.pi*t/self.T-1.5394)-0.021983*np.sin(18*np.pi*t/self.T+0.95617)-0.013072*np.sin(20*np.pi*t/self.T-0.022417)+0.0037028*np.sin(22*np.pi*t/self.T-1.4146)-0.013973*np.sin(24*np.pi*t/self.T+0.77416)-0.012423*np.sin(26*np.pi*t/self.T-0.46511)+0.0040098*np.sin(28*np.pi*t/self.T+0.95145)-0.0059704*np.sin(30*np.pi*t/self.T+0.86369)-0.0073439*np.sin(32*np.pi*t/self.T-0.64769)+0.0037006*np.sin(34*np.pi*t/self.T+0.74663)-0.0032069*np.sin(36*np.pi*t/self.T+0.85926)-0.0048171*np.sin(38*np.pi*t/self.T-1.0306)+0.0040403*np.sin(40*np.pi*t/self.T+0.28009)-0.0032409*np.sin(42*np.pi*t/self.T+1.202)-0.0032517*np.sin(44*np.pi*t/self.T-0.93316)+0.0029112*np.sin(46*np.pi*t/self.T+0.21405)-0.0022708*np.sin(48*np.pi*t/self.T+1.1869)-0.0021566*np.sin(50*np.pi*t/self.T-1.1574)+0.0025511*np.sin(52*np.pi*t/self.T-0.12915)-0.0024448*np.sin(54*np.pi*t/self.T+1.1185)-0.0019032*np.sin(56*np.pi*t/self.T-0.99244)+0.0019476*np.sin(58*np.pi*t/self.T-0.059885)-0.0019477*np.sin(60*np.pi*t/self.T+1.1655)-0.0014545*np.sin(62*np.pi*t/self.T-0.85829)+0.0013979*np.sin(64*np.pi*t/self.T+0.042912)-0.0014305*np.sin(66*np.pi*t/self.T+1.2439)-0.0010775*np.sin(68*np.pi*t/self.T-0.79464)+0.0010368*np.sin(70*np.pi*t/self.T-0.0043058)-0.0012162*np.sin(72*np.pi*t/self.T+1.211)-0.00095707*np.sin(74*np.pi*t/self.T-0.66203)+0.00077733*np.sin(76*np.pi*t/self.T+0.25642)-0.00092407*np.sin(78*np.pi*t/self.T+1.3954)-0.00079585*np.sin(80*np.pi*t/self.T-0.49973))
+		
+class AoBifInflow(BoundaryConditionType1):
+	"""
+	Creates the inflow used in test Of Single Aorta used in section 3.4 in Comparison1Dscheme 
+	"""
+	def __init__(self):
+		
+		BoundaryConditionType1.__init__(self)
+				
+		# # additional variables for this function
+		self.T = 1.1
+	
+	def function1(self, t, t0, pulsNum):
+
+		return (7.9853e-06+2.6617e-05*np.sin(2*np.pi*t/self.T+0.29498)+2.3616e-05*np.sin(4*np.pi*t/self.T-1.1403)-1.9016e-05*np.sin(6*np.pi*t/self.T+0.40435)-8.5899e-06*np.sin(8*np.pi*t/self.T-1.1892)-2.436e-06*np.sin(10*np.pi*t/self.T-1.4918)+1.4905e-06*np.sin(12*np.pi*t/self.T+1.0536)+1.3581e-06*np.sin(14*np.pi*t/self.T-0.47666)-6.3031e-07*np.sin(16*np.pi*t/self.T+0.93768)-4.5335e-07*np.sin(18*np.pi*t/self.T-0.79472)-4.5184e-07*np.sin(20*np.pi*t/self.T-1.4095)-5.6583e-07*np.sin(22*np.pi*t/self.T-1.3629)+4.9522e-07*np.sin(24*np.pi*t/self.T+0.52495)+1.3049e-07*np.sin(26*np.pi*t/self.T-0.97261)-4.1072e-08*np.sin(28*np.pi*t/self.T-0.15685)-2.4182e-07*np.sin(30*np.pi*t/self.T-1.4052)-6.6217e-08*np.sin(32*np.pi*t/self.T-1.3785)-1.5511e-07*np.sin(34*np.pi*t/self.T-1.2927)+2.2149e-07*np.sin(36*np.pi*t/self.T+0.68178)+6.7621e-08*np.sin(38*np.pi*t/self.T-0.98825)+1.0973e-07*np.sin(40*np.pi*t/self.T+1.4327)-2.5559e-08*np.sin(42*np.pi*t/self.T-1.2372)-3.5079e-08*np.sin(44*np.pi*t/self.T+0.2328))
+			
+			
 class PhysiologicalFunction(BoundaryConditionType1):
 	"""	
 	Boundary profile - type 1
