214b4071b571e176dd5909ec83f6ad04f1f50818
Vinzenz <vinzenz.g.eck@ntnu.no>
2015-11-23 12:28:11 +0100

    added simulation status bar

--- SolverLib/class1DflowSolver.py
+++ SolverLib/class1DflowSolver.py
@@ -611,7 +611,18 @@ class FlowSolver(cSBO.StarfishBaseObject):
 
         reflectionCoefficientCount = 0
         maxRef = 0
-
+        
+        #initialize loading bar
+        write = sys.stdout.write
+        nElements = 35
+        loadingBarElementCount = 1
+        spaces = ''.join([' ' for i in xrange(nElements)])
+        loadingBar = ''.join(['[',spaces,']'])
+        write(loadingBar)
+        sys.stdout.flush()
+        backspacing = ''.join(['\b' for i in xrange(nElements+1)])
+        write(backspacing)
+        
         if self.cycleMode == False:
             # original
             for n in xrange(self.nTSteps):
@@ -628,7 +639,12 @@ class FlowSolver(cSBO.StarfishBaseObject):
                         print "Success in saving solution data file. Reraising Exception"
                         raise # TODO: why does self.exception() not force the program to quit?
                         # self.exception()
-
+                
+                if divmod(n,(self.nTSteps/nElements))[0] == loadingBarElementCount:
+                    loadingBarElementCount=loadingBarElementCount+1
+                    write("#")
+                    sys.stdout.flush()
+                
         ## to be concentrated with original cycle mode !!
         else:
             # steady state variables
